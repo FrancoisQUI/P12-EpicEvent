85306b401ff0a17a5d48665f688ccc2870d33f1c
FranQUI <fquivron@gmail.com>
2022-02-17 09:25:10 +0100

    Add form in admin site for each model

--- customer/admin.py
+++ customer/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Networks
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Customer, CustomerAdmin)
+
+
+class NetworksAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Networks, NetworksAdmin)
