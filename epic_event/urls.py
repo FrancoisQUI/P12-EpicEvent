7ff8f16939121425673887e23cdd728ca559ab61
FranQUI <fquivron@gmail.com>
2022-02-23 10:58:38 +0100

    Add Views and Routes For Env app

--- epic_event/urls.py
+++ epic_event/urls.py
@@ -4,6 +4,7 @@ from rest_framework import routers
 
 from customer.views import CustomerViewSet
 from conversion.views import ConversionViewSet
+from event.views import EventViewSet
 
 customer_router = routers.SimpleRouter()
 customer_router.register(r'customer', CustomerViewSet, basename="customer")
@@ -11,9 +12,13 @@ customer_router.register(r'customer', CustomerViewSet, basename="customer")
 conversion_router = routers.SimpleRouter()
 conversion_router.register(r'conversion', ConversionViewSet, basename="conversion")
 
+event_router = routers.SimpleRouter()
+event_router.register(r'event', EventViewSet, basename="event")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += customer_router.urls
 urlpatterns += conversion_router.urls
+urlpatterns += event_router.urls
--- event/views.py
+++ event/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import DjangoObjectPermissions
+from drf_roles.mixins import RoleViewSetMixin
 
-# Create your views here.
+from .serializers import EventSerializer
+from .models import Event
+
+
+class EventViewSet(RoleViewSetMixin, ModelViewSet):
+    model = Event
+    serializer_class = EventSerializer
+    permission_classes = [DjangoObjectPermissions]
+
+    def get_queryset_for_admin(self) -> object:
+        return Event.objects.all()
+
+    def get_queryset_for_management(self) -> object:
+        return Event.objects.all()
+
+    def get_queryset_for_sales(self):
+        return Event.objects.all()
+
+    def get_queryset_for_support(self):
+        return Event.objects.filter(assigned_user=self.request.user).distinct()
+
+    def get_queryset(self):
+        return Event.objects.none()
