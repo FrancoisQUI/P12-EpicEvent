56bf6bdfff63cd183f9690efef1174ea63b85fd2
FranQUI <fquivron@gmail.com>
2022-02-23 10:37:07 +0100

    Add Views and Routes For Conversion app

--- conversion/views.py
+++ conversion/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import DjangoObjectPermissions
+from drf_roles.mixins import RoleViewSetMixin
 
-# Create your views here.
+from .serializers import ConversionSerializer
+from .models import Conversion
+
+
+class ConversionViewSet(RoleViewSetMixin, ModelViewSet):
+    model = Conversion
+    serializer_class = ConversionSerializer
+    permission_classes = [DjangoObjectPermissions]
+
+    def get_queryset_for_admin(self) -> object:
+        return Conversion.objects.all()
+
+    def get_queryset_for_management(self) -> object:
+        return Conversion.objects.all()
+
+    def get_queryset_for_sales(self):
+        return Conversion.objects.all()
+
+    def get_queryset_for_support(self):
+        return Conversion.objects.none()
+
+    def get_queryset(self):
+        return Conversion.objects.none()
--- epic_event/urls.py
+++ epic_event/urls.py
@@ -3,12 +3,17 @@ from django.urls import path
 from rest_framework import routers
 
 from customer.views import CustomerViewSet
+from conversion.views import ConversionViewSet
 
 customer_router = routers.SimpleRouter()
 customer_router.register(r'customer', CustomerViewSet, basename="customer")
+
+conversion_router = routers.SimpleRouter()
+conversion_router.register(r'conversion', ConversionViewSet, basename="conversion")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 urlpatterns += customer_router.urls
+urlpatterns += conversion_router.urls
